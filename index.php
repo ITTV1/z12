7a48099756c3fecb48b9dbc45dd6b9bb2113f3f4
ITTV1 <62187261+ITTV1@users.noreply.github.com>
2020-04-14 20:49:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//Работа с путями
+//
+//
+//
+//
+//string basename(string $p, string $suff)
+
+
+//echo basename("/home/php/ittv.txt");//ittv.txt
+
+//echo basename("/.");//.
+
+//echo basename("/");//ничего не выводит. функция не вывела ничего
+
+//echo basename("/./");//.
+
+
+
+echo basename("/home/php/ittv.txt", ".txt");//ittv
+
+
+
+
+?>
